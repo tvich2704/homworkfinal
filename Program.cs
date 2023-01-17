9746aeaa681ef03efb8ac5a1542e98d3f563fd48
krasnobaev.a.a <tvich2704@gmail.com>
2023-01-17 15:41:24 +0500

    первый комит

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string[] array1 = {"Hello", "2", "world", ":-)"};
+string[] array2 = {"1234", "1567", "-2","computer science"};
+string[] array3 = {"Russia", "Denmark", "Kazan"};
+Console.WriteLine($"[{string.Join(", ", array1)}]->[{string.Join(", ", answer(array1))}]");
+Console.WriteLine($"[{string.Join(", ", array2)}]->[{string.Join(", ", answer(array2))}]");
+Console.WriteLine($"[{string.Join(", ", array3)}]->[{string.Join(", ", answer(array3))}]");
+
+
+//для проверки с клавиатуры по желанию
+// Console.Write("Введите с клавиатуры элементы массива через пробел:");
+// string[] str = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+// Console.WriteLine($"[{string.Join(", ", str)}]->[{string.Join(", ", answer(str))}]");
+
+
